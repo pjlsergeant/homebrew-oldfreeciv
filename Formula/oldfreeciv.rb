83a7b86182add92d54f6f27f167c0523d82cc4b4
Peter Sergeant <pete@clueball.com>
2021-05-15 13:59:36 +0700

    Amended name

--- Formula/oldfreeciv.rb
+++ Formula/oldfreeciv.rb
@@ -1,4 +1,4 @@
-class Freeciv < Formula
+class OldFreeciv < Formula
   desc "Free and Open Source empire-building strategy game"
   homepage "http://freeciv.org"
   url "https://downloads.sourceforge.net/project/freeciv/Freeciv%202.6/2.6.3/freeciv-2.6.3.tar.bz2"
